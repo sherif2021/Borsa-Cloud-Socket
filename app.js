57462f700bd3430dca99e5fd2091097eb89e658b
Sherif Sobhy <63163881+sherif2021@users.noreply.github.com>
2024-02-18 09:28:31 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+const online = new Map()
+
+io.on('connection', (socket) => {
+    const userId = parseInt(socket.handshake.headers.authorization);
+    if (userId) {
+        socket.userId = userId
+        if (online.has(userId)) {
+            online.get(userId).sockets.push(socket)
+        }
+        else {
+            online.set(userId, { 'sockets': [socket] })
+        }
+        console.log(`${userId} user connected`);
+        socket.on('disconnect', () => closeConnection(socket));
+        socket.on('new-message', (text) => onNewMessage(text));
+
+    } else {
+        socket.disconnect();
+    }
+});
+
+function closeConnection(socket) {
+
+    try {
+
+        if (socket.userId && online.has(socket.userId)) {
+
+            const user = online.get(socket.userId)
+
+            const index = user.sockets.indexOf(socket)
+
+            if (index !== -1) {
+                user.sockets.splice(index, 1);
+            }
+
+            if (user.sockets.length == 0) {
+                online.delete(socket.userId)
+            }
+        }
+        console.log(`${socket.userId} disconnect`)
+        socket.disconnect();
+
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+function onNewMessage(data) {
+
+    const { sender_id, contact_id } = data;
+    if (sender_id && online.has(sender_id)) {
+        const user = online.get(sender_id)
+        user.sockets.forEach(socket => {
+            socket.emit('new-message', data)
+        })
+    } else console.log('here');
+    if (contact_id && online.has(contact_id)) {
+        const user = online.get(contact_id)
+        user.sockets.forEach(socket => {
+            socket.emit('new-message', data)
+        })
+    } else console.log('here2');
+}
+server.listen(8000, () => {
+    console.log('listening on *:8000');
+});
